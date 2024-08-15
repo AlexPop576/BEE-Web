0bc624c6cb024e0b6828d3de30c7e075e902eba1
AlexPop576 <apcode576@gmail.com>
2024-08-16 01:40:35 +0300

    update

--- src/Pages/vote1.jsx
+++ src/Pages/vote1.jsx
@@ -1,12 +1,20 @@
 import { Link } from 'react-router-dom';
 import Button from '../components/Button/Button.jsx'
 import ProgressBar from '../components/ProgressBar/ProgressBar.jsx';
+import VoteTile from '../components/VoteTile/VoteTile.jsx';
 
 export function Vote1() {
     return (
         <div className="App">
             <ProgressBar activeSteps = {4}/>
-            Vote1
+            <div className='grid-container'>
+                <VoteTile/>
+                <VoteTile/>
+                <VoteTile/>
+                <VoteTile/>
+                <VoteTile/>
+                <VoteTile/>
+            </div>
             <Link to="/vote2">
                 <Button text = "Continuă"/>
             </Link>
---
+++ src/components/VoteTile/VoteTile.jsx
@@ -0,0 +1,10 @@
+import './VoteTile.css'
+
+export default function VoteTile () {
+    return (
+        <div className='vote-tile'>
+            <p id='party'>Partidul</p>
+            <p id='name'>Claus</p>
+        </div>
+    );
+}
